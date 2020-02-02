c2f978ccfe789562ea03bcad2243aee529ef2201
awhyrick <54607614+awhyrick@users.noreply.github.com>
2020-02-02 11:38:48 -0600

    Update settings.py

--- clientms/clientms/settings.py
+++ clientms/clientms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*vy2s)yqt)ceesa+=l(3!6lyfuiv9*^#7)oy9^w1wasu+lef@#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
